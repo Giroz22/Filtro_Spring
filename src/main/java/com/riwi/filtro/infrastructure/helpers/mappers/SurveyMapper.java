0f30f287714f8fdecf574867042a24d36b82c143
Alejandro Giraldo S <giraldosernaalejandro16@gmail.com>
2024-06-04 13:49:37 -0500

    task-dev-10 and task-dev-11

--- src/main/java/com/riwi/filtro/infrastructure/helpers/mappers/SurveyMapper.java
+++ src/main/java/com/riwi/filtro/infrastructure/helpers/mappers/SurveyMapper.java
@@ -1,13 +1,20 @@
 package com.riwi.filtro.infrastructure.helpers.mappers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.riwi.filtro.api.dto.request.SurveyCreatedRequest;
 import com.riwi.filtro.api.dto.response.SurveyResponse;
 import com.riwi.filtro.domain.entitties.Survey;
 
+import lombok.AllArgsConstructor;
+
 @Component
+@AllArgsConstructor
 public class SurveyMapper implements IMapperBase<Survey, SurveyCreatedRequest, SurveyResponse>{
+
+    @Autowired
+    private final QuestionMapper questionMapper;
 
     @Override
     public Survey requestToEntity(SurveyCreatedRequest request) {
@@ -17,8 +24,15 @@ public class SurveyMapper implements IMapperBase<Survey, SurveyCreatedRequest, S
 
     @Override
     public SurveyResponse entityToResponse(Survey entity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'entityToResponse'");
+        return Mapper.sourceToTarget(entity, 
+            SurveyResponse.builder()
+            .questions(
+                entity.getQuestions().stream().map(
+                    (questions)-> questionMapper.entityToResponse(questions)
+                ).toList()
+            )
+            .build()
+        );
     }
     
 }
